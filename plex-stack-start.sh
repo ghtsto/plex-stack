6ff7a6c546c276026414db63b94cddd274c12498
ghtsto <22155165+ghtsto@users.noreply.github.com>
2020-04-20 12:34:52 -0700

    updated containers to use volumes; other improvements

--- plex-stack-start.sh
+++ plex-stack-start.sh
@@ -1,29 +1,88 @@
 #!/bin/bash
 
 # get all container names from docker-compose.yml
-IFS=' ' read -r -a containers <<< $(grep container_name docker-compose.yml | cut -d ':' -f2 | xargs)
+IFS=' ' read -r -a CONTAINERS <<< $(grep container_name docker-compose.yml | cut -d ':' -f2 | xargs)
+
+# get variables from .env
+export $(grep -v '^#' .env | xargs -d '\n')
 
 # set plexdrive data path which will be used for checking if it is ready
-PLEXDRIVE_PATH=$(grep DATA_DIR .env | cut -d '=' -f2)/plexdrive/data/
+PLEXDRIVE_PATH=./plexdrive/unionfs/
 
-# start every container except plex, sonarr, radarr, lidarr or rclone as they depend on the plexdrive /data mount
-for container in "${containers[@]}"
+# set the docker-compose project name
+PROJECT=$COMPOSE_PROJECT_NAME
+
+# create docker network if it does not exist
+docker network inspect ${PROJECT} >/dev/null 2>&1 || docker network create ${PROJECT}
+
+# create encfs volume, create if it does not exist and copy encfs config files
+docker volume inspect ${PROJECT}_encfs >/dev/null 2>&1 || docker volume create ${PROJECT}_encfs
+./bin/docker-volume-cp ./encfs/. ${PROJECT}_encfs:.
+
+# create docker volumes for containers if they do not exist and copy config files
+for CONTAINER in "${CONTAINERS[@]}"
 do
-  if [ ! "$container" = "plex" ] && [ ! "$container" = "sonarr" ] && [ ! "$container" = "radarr" ] && [ ! "$container" = "lidarr" ] && [ ! "$container" = "rclone" ]; then
-    docker-compose up -d "$container"
+  case $CONTAINER in
+    traefik)
+      docker volume inspect ${PROJECT}_$CONTAINER >/dev/null 2>&1 || docker volume create ${PROJECT}_$CONTAINER
+      cp ./$CONTAINER/dynamic_conf.toml.example ./$CONTAINER/dynamic_conf.toml
+      sed -i "s|HTPASSWD|$HTPASSWD|g" ./$CONTAINER/dynamic_conf.toml
+      sed -i "s|DOMAIN|$DOMAIN|g" ./$CONTAINER/dynamic_conf.toml
+      cp ./$CONTAINER/traefik.toml.example ./$CONTAINER/traefik.toml
+      sed -i "s|ACME_EMAIL|$ACME_EMAIL|g" ./$CONTAINER/traefik.toml
+      ./bin/docker-volume-cp ./$CONTAINER/. ${PROJECT}_$CONTAINER:.
+      rm ./$CONTAINER/dynamic_conf.toml
+      rm ./$CONTAINER/traefik.toml
+      ;;
+    rclone)
+      docker volume inspect ${PROJECT}_$CONTAINER-logs >/dev/null 2>&1 || docker volume create ${PROJECT}_$CONTAINER-logs
+      docker volume inspect ${PROJECT}_$CONTAINER-config >/dev/null 2>&1 || docker volume create ${PROJECT}_$CONTAINER-config
+      ./bin/docker-volume-cp ./$CONTAINER/. ${PROJECT}_$CONTAINER-config:.
+      ;;
+    plex)
+      docker volume inspect ${PROJECT}_$CONTAINER-config >/dev/null 2>&1 || docker volume create ${PROJECT}_$CONTAINER-config
+      docker volume inspect ${PROJECT}_$CONTAINER-transcode >/dev/null 2>&1 || docker volume create ${PROJECT}_$CONTAINER-transcode
+      ;;
+    sabnzbd)
+      docker volume inspect ${PROJECT}_$CONTAINER-config >/dev/null 2>&1 || docker volume create ${PROJECT}_$CONTAINER-config
+      docker volume inspect ${PROJECT}_$CONTAINER-downloads >/dev/null 2>&1 || docker volume create ${PROJECT}_$CONTAINER-downloads
+      docker volume inspect ${PROJECT}_$CONTAINER-incomplete >/dev/null 2>&1 || docker volume create ${PROJECT}_$CONTAINER-incomplete
+      ;;
+    deluge|\
+    jackett)
+      docker volume inspect ${PROJECT}_$CONTAINER-config >/dev/null 2>&1 || docker volume create ${PROJECT}_$CONTAINER-config
+      docker volume inspect ${PROJECT}_$CONTAINER-downloads >/dev/null 2>&1 || docker volume create ${PROJECT}_$CONTAINER-downloads
+      ;;
+    portainer|\
+    tautulli|\
+    ombi|\
+    sonarr|\
+    organizr|\
+    rclone|\
+    radarr|\
+    lidarr|\
+    plexdrive)
+      docker volume inspect ${PROJECT}_$CONTAINER >/dev/null 2>&1 || docker volume create ${PROJECT}_$CONTAINER
+      ;;
+  esac
+
+  if [ ! $CONTAINER = "plex" ] && \
+     [ ! $CONTAINER = "sonarr" ] && \
+     [ ! $CONTAINER = "radarr" ] && \
+     [ ! $CONTAINER = "lidarr" ] && \
+     [ ! $CONTAINER = "rclone" ]; then
+    docker-compose up -d $CONTAINER
   fi
 done
 
-# while the plexdrive /data mount is inaccessible, sleep
+# while the plexdrive /unionfs mount is inaccessible, sleep
 while [ ! "$(ls -A $PLEXDRIVE_PATH)" ]
 do
   printf "waiting for $PLEXDRIVE_PATH to mount\n"
   sleep 2
 done
 
-# after plexdrive /data mount is accessible, bring remaining containers up
-docker-compose up -d plex
-docker-compose up -d sonarr
-docker-compose up -d radarr
-docker-compose up -d lidarr
-docker-compose up -d rclone
+for CONTAINER in "${CONTAINERS[@]}"
+do
+  docker-compose up -d $CONTAINER
+done
